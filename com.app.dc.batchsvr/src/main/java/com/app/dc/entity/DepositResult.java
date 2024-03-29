a8ad53691edf1d48136186bfd14371b913ff4b35
troy <>
2024-03-29 10:18:21 +0800

    deposition

---
+++ com.app.dc.batchsvr/src/main/java/com/app/dc/entity/DepositResult.java
@@ -0,0 +1,15 @@
+package com.app.dc.entity;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author
+ * @Date
+ **/
+@Data
+public class DepositResult {
+
+    private DepositionData data;
+
+}
---
+++ com.app.dc.batchsvr/src/main/java/com/app/dc/entity/DepositSearchParam.java
@@ -0,0 +1,16 @@
+package com.app.dc.entity;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author
+ * @Date
+ **/
+@Data
+public class DepositSearchParam {
+
+    private String query;
+    private Object variables;
+
+}
